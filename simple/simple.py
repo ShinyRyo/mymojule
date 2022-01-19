d98b6e74347a73c1e8f132927c03764af0e15dda
ShinyRyo <tan0ry0shiny@gmail.com>
2022-01-19 17:31:19 +0900

    3ms

--- simple/simple.py
+++ simple/simple.py
@@ -1,3 +1,7 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
 def perceptron(x1, x2, w1, w2, b):
     a = w1 * x1 + w2 * x2 + b
     return a
@@ -12,3 +16,24 @@ def step_function(x):
         #a = [1 if i>0 else 0 for i in x]
         a = np.array(x > 0, dtype=np.int)
     return a
+
+def plot3d(x1, x2, w1, w2, b):
+    #3Dで描写するための空間の準備
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    ax.set_xlabel("x1")
+    ax.set_ylabel("x2")
+    ax.set_zlabel("y")
+    #3Dで描写するためのデータの準備
+    x1_range=x1
+    x2_range=x2
+    x1 = np.arange(x1_range.min(), x1_range.max(), 0.01) #x1を0から1の範囲で0.01ずつ刻んでいきます
+    x2 = np.arange(x2_range.min(), x1_range.max(), 0.01) #x2を0から1の範囲で0.01ずつ刻んでいきます
+    X, Y = np.meshgrid(x1, x2)
+    ##AND回路の出力を3Dで描写するためのデータを算出
+    y=perceptron(X,Y, w1, w2, b)#AND(X,Y)
+    Z=step_function(y)
+
+    ax.plot_wireframe(X, Y, Z)
+    plt.show()
