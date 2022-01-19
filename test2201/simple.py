3539329fa0a07db5fca9592bfaf086844c962c99
ShinyRyo <tan0ry0shiny@gmail.com>
2022-01-19 17:36:01 +0900

    fixname

--- test2201/simple.py
+++ test2201/simple.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
@@ -37,3 +38,13 @@ def plot3d(x1, x2, w1, w2, b):
 
     ax.plot_wireframe(X, Y, Z)
     plt.show()
+
+def plot2d(x1, x2, w1, w2, b):
+    df=pd.DataFrame({"x1":x1})
+    df["x2"]=x2
+    try:
+        df['sum2D']=-w1/w2*df['x1']-(b/w2)
+    except:
+        df['sum2D']=-w1/w2*df['x1']-(b/w2).item()
+    ax=df[['x1','x2']].plot(kind='scatter', x='x1', y='x2',grid=True )
+    df[['x1','sum2D']].plot(ax=ax, kind='line', x='x1', y='sum2D',grid=True )
