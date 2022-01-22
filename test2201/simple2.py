c7d75267d0b226f6b2257c8b5e8b62edbd4da1b1
ShinyRyo <58499405+ShinyRyo@users.noreply.github.com>
2022-01-22 16:11:16 +0900

    softmax

--- test2201/simple2.py
+++ test2201/simple2.py
@@ -1,11 +1,20 @@
+import numpy as np
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+
 class perceptron:
     # パラメータの初期化
-    def __init__(self, w1,w2,b):
+    def __init__(self, input_size, output_size):
         import numpy as np      
         self.params = {}
-        self.params['W1'] = np.array([[w1],[w2]])
-        self.params['b1'] = np.array([b])
+        self.params['W1'] = np.random.randn(input_size,output_size)
+        self.params['b1'] = np.zeros(output_size)
+        # レイヤの生成
+        self.layers = OrderedDict()
 
+        self.lastLayer = SoftmaxWithLoss()
     # 順伝播
     def forward(self, x):
         W1 = self.params['W1']
@@ -14,3 +23,65 @@ class perceptron:
         a1 = np.dot(x, W1) + b1
         y = a1     
         return y
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        
+        return 
+    # x:入力データ, t:教師データ
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size: # 教師データがone-hot-vectorの場合
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+        
+        return dx
