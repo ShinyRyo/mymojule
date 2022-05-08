e031a7bfa5c995b8589a187b8beb66e592682954
ShinyRyo <58499405+ShinyRyo@users.noreply.github.com>
2022-05-08 12:32:24 +0900

    finish2

--- MyLGBM/MyLGBM.py
+++ MyLGBM/MyLGBM.py
@@ -63,3 +63,19 @@ class MyLGBM:
                 # dic={i:lgb_model}
                 # lgb_models.update(dic)
         return lgb_model
+
+    #説明変数と目的変数のリストを抽出
+    def devide_tr_ob(train, test):
+        train_labels=list(train.columns)
+        test_labels=list(test.columns)
+        target=train_labels
+        feats=test_labels
+        catfeats=list(train.select_dtypes(include= "object").columns)
+        for i in test_labels:
+            target.remove(i)
+        return feats, catfeats, target
+    #訓練データとテストデータの分割
+    def devide_train_test(data, feats, target):
+        train=data[data[target[0]].notna()]
+        test=data[feats]
+        return train, test
